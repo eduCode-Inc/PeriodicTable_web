ab5320cd3123757b0bad333ccee3685ae7c5e5fb
Medhansh <87754879+Medhansh-shah@users.noreply.github.com>
2021-11-23 15:44:07 +0530

    changed some stuff

--- output.js
+++ output.js
@@ -1,43 +1,7 @@
-function svk();
+function askForInput()
 { 
-     document.write ("35-Bromine" + "<br>" );
-     document.write ("36-Krypton" + "<br>");
-     document.write ("37-Rubidium"+ "<br>");
-     document.write ("38-Strontium"+ "<br>");
-     document.write ("39-Yttrium"+ "<br>");
-     document.write ("40-Zirconium"+ "<br>");
-     document.write ("41-Niobium"+ "<br>");
-     document.write ("42-Molybdenum"+ "<br>");
-     document.write ("43-Technetium"+ "<br>");
-     document.write ("44-Ruthenium"+ "<br>");
-     document.write ("45-Rhodium"+ "<br>");
-     document.write ("46-Palladium"+ "<br>");
-     document.write ("47-Silver"+ "<br>");
-     document.write ("48-Cadmium"+ "<br>");
-     document.write ("49-Indium"+ "<br>");
-     document.write ("50-Tin"+ "<br>");
-     document.write ("51-Antimony"+ "<br>");
-     document.write ("52-Tellurium"+ "<br>");
-     document.write ("53-Iodine"+ "<br>");
-     document.write ("54-Xenon"+ "<br>");
-     document.write ("55-Caesium"+ "<br>");
-     document.write ("56-Barium"+ "<br>");
-     document.write ("57-Lathanum"+ "<br>");
-     document.write ("58-Cerium"+ "<br>");
-     document.write ("59-Praseodymium"+ "<br>");
-     document.write ("60-Neodymium"+ "<br>");
-     document.write ( "61-Promethium" + "<br>");
-     document.write ("62-Samarium"+ "<br>");
-     document.write("63-Europium"+ "<br>");
-     document.write ("64-Gadalinium"+ "<br>");
-     document.write ("65-Terbium"+ "<br>");
-     document.write ("66-Dysprasium"+ "<br>");
-     document.write ("67-Halmium"+ "<br>");
-     document.write ("68-Erbium "+ "<br>");
-     document.write ("69-Thulium "+ "<br>");
-     document.write ("70-Ytterbium"+ "<br>");
     a1 = prompt("Enter Atomic Number " )
-    switch(a1 )
+    switch(a1)
     {
         case "35":
             {
@@ -453,6 +417,7 @@ function svk();
              document.write ("Valency:2,3,4" + "<br>");
               break;
             }
+            //add rest of the cases here
     
     }
   
